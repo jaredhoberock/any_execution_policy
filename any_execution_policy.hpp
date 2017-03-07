db64006b70548199087b93954c16ccf8c7318bd5
Jared Hoberock <jaredhoberock@gmail.com>
2017-03-07 15:00:16 -0600

    Implement storage with experimental::any

--- any_execution_policy.hpp
+++ any_execution_policy.hpp
@@ -1,2 +1,24 @@
 #pragma once
 
+#include <agency/agency.hpp>
+#include <experimental/any>
+#include <utility>
+
+class any_execution_policy
+{
+  public:
+    template<class ExecutionPolicy,
+             __AGENCY_REQUIRES(agency::is_execution_policy<typename std::decay<ExecutionPolicy>::type>::value)>
+    any_execution_policy(ExecutionPolicy&& policy)
+      : policy_(std::forward<ExecutionPolicy>(policy))
+    {}
+
+    const std::type_info& type() const
+    {
+      return policy_.type();
+    }
+
+  private:
+    std::experimental::any policy_;
+};
+
--- demo.cpp
+++ demo.cpp
@@ -1,7 +1,16 @@
 #include <agency/agency.hpp>
+#include "async_reduce/reduce.hpp"
+#include "any_execution_policy.hpp"
+#include <iostream>
 
 int main()
 {
+  any_execution_policy policy = agency::par;
+
+  assert(policy.type() == typeid(agency::par));
+
+  std::cout << "OK" << std::endl;
+
   return 0;
 }
 
